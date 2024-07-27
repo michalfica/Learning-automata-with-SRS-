f03cca340725a21392fac5acf36351b0d5595a79
michalfica <michalfica125@gmail.com>
2024-07-27 11:49:06 +0200

    random_route of the Mealy machine

--- Mealymachine.py
+++ Mealymachine.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class MealyMachine:
     """konwencja:
     Q                   - liczba stanów maszyny,
@@ -8,20 +11,47 @@ class MealyMachine:
 
     def __init__(self, Q=0, input_signs=None, output_signs=None, λ=dict(), δ=dict()):
         self.Q = Q
-        self.input_signs = input_signs
-        self.output_signs = output_signs
+        self.input_signs = sorted(input_signs)  # sortowanie by móc porównywac
+        self.output_signs = sorted(output_signs)
         self.λ = λ
         self.δ = δ
 
     def __str__(self):
-        return f"I = {self.input_signs}, O = {self.output_signs}"
+        return f"Mealy Machine amount of states = {self.Q}, I = {self.input_signs}, O = {self.output_signs}"
+
+    def random_route(self, n=10):
+        w, v, q = "".join(np.random.choice(self.input_signs, n, replace=True)), "", 0
+        for a in w:
+            v += self.λ[(q, a)]
+            q = self.δ[(q, a)]
+        return (w, v)
 
     def print_transitions(self):
         for q in range(self.Q):
             for a in self.input_signs:
+                assert (q, a) in self.λ and (
+                    q,
+                    a,
+                ) in self.δ, (
+                    "nie ma taiego przejścia w maszynie, potencjalnie zły alfabet!"
+                )
                 print(f"({q},{a}) --> '{self.λ[(q,a)]}',{self.δ[(q,a)]}")
 
-    # TO DO:
-    # def _print(self)        <- wypisać krawędzie maszyny jakoś zgrabnie (albo jak kolwiek)
-    #
-    # def __eq__(self, other) <- sprawdzić czy dwie maszyny są równoważne, (sprawdzając czy stany poczatkowe są, zgodnie z reguła)
+    # TO DO: NIE WIEM JAK SPRAWDZAĆ RÓWNOWAŻNOŚĆ DWÓCH STANÓW (żęby sie nie zapętlać w ∞)
+    # def __eq__(self, other):
+    #     if not (
+    #         self.input_signs == other.input_signs
+    #         and self.output_signs == other.output_signs
+    #     ):
+    #         return 1  # nie zgadzaja sie alfabety wiec na pewno nie są równoważne
+
+    #     def state_equivalance(q1, q2):
+    #         for a in self.input_signs:
+    #             if not (
+    #                 self.λ[(q1, a)] == other.λ[(q2, a)]
+    #                 and state_equivalance(self.δ[(q1, a)], other.δ[(q2, a)])
+    #             ):
+    #                 return False
+    #         return True
+
+    #     return state_equivalance(0, 0)
