c0bb6728d0dce6201ee1e4f16611f986b42ab126
michalfica <michalfica125@gmail.com>
2024-09-13 09:55:56 +0200

    state transitions, not tested

--- PatternSwap/PatternSwap.py
+++ PatternSwap/PatternSwap.py
@@ -9,6 +9,8 @@ class PatternSwap(MealyMachine):
     n        - amount of pairs of pattens
     """
 
+    NO_PRINTNIG = -1
+
     def __init__(self, patterns, insigns, outsigns):
         def compute_number_of_states():
             n = np.prod([len(x) for x in patterns])
@@ -20,11 +22,68 @@ class PatternSwap(MealyMachine):
         )
         self.patterns = patterns
         self.n = len(self.patterns)
+        self.state_mapping = dict()
         self._compute_state_transitions()
         self._compte_output_function()
 
     def _compute_state_transitions(self):
-        pass
+        def find_new_state(x, a):
+            printing, letter = x[self.n], x[self.n + 1]
+            if printing != self.NO_PRINTNIG and letter == len(
+                self.patterns[printing][1]
+            ):
+                printing = self.NO_PRINTNIG
+
+            words = [self.patterns[i][0][: x[i]] + a for i in range(self.n)]
+            state = []
+            for i in range(self.n):
+                # szukam max prefiksu słowa self.patterns[i][0], które jest sufiksem słowa words[i]
+                prefixes = set(
+                    [
+                        self.patterns[i][0][:j]
+                        for j in range(len(self.patterns[i][0]) + 1)
+                    ]
+                )
+                suffixes = [words[i][j:] for j in range(len(words[i]) + 1)]
+                for s in suffixes:
+                    if s in prefixes:
+                        state.append(len(s) % (len(self.patterns[i][0]) + 1))
+                        break
+
+            if printing == self.NO_PRINTNIG:
+                full_patterns = [
+                    i for i in range(self.n) if s[i] == len(self.patterns[i][1])
+                ]
+                assert (
+                    full_patterns <= 1
+                ), "wykryto kilka wzorców naraz w jednym miejscu - niewiadomo co wypisać!"
+                if full_patterns:
+                    printing = full_patterns[0]
+                state.extend([printing, 0])
+            else:
+                state.extend([printing, letter + 1])
+
+            return self.state_mapping[tuple(state)]
+
+        xs = [range(len(x[0]) + 1) for x in self.patterns]
+        xs.append(range(self.n + 1))
+        xs.append(range(max([len(x[0]) for x in self.patterns])))
+
+        match self.n:
+            case 1:
+                nested_loop1, nested_loop2 = product(xs[0], xs[1], xs[2]), product(
+                    xs[0], xs[1], xs[2]
+                )
+            case _:
+                assert False, "too much patterns, maximum possible is only 1!"
+
+        cnt = 0
+        for x in nested_loop1:
+            self.state_mapping[x] = cnt
+            cnt += 1
+        for x in nested_loop2:
+            for a in self.input_signs:
+                self.δ[(self.state_mapping[x], a)] = find_new_state(x, a)
 
     def _compte_output_function(self):
         pass
