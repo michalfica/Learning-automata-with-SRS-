77bfa75df757b927d364a35131ef2c45757a18c4
michalfica <michalfica125@gmail.com>
2024-09-17 11:21:28 +0200

    patternsswap class seems to work

--- PatternSwap/PatternSwap.py
+++ PatternSwap/PatternSwap.py
@@ -10,18 +10,19 @@ class PatternSwap(MealyMachine):
 
     The state in the machine is a n-element tuple, on the ith coordniate there is:
         (*) 0 <= i < n   - length of max prefix of i-th pattern, ending on curently read word,
-        (*) n <= i < n+1 - the number of currently printed pattern            or 0          otherwise,
-        (*) i == n+1     - the number of the currently printed pattern letter or [whatever] otherwise.
+        (*) n <= i < n+1 - the number of currently printed pattern (NUMBERED FROM 1 TO N)    or 0          otherwise,
+        (*) i == n+1     - the number of the currently printed pattern letter                or [whatever] otherwise.
 
     Note on the 'language'/relation defined b PatternSwap machines:
-    patterns recognised in text by the machine are DISJOINT, swaps can not overlap.
+    patterns recognised in text by the machine are DISJOINT, swaps can not overlap, (printing next pattern-swap can take only place after previous printing will be finised)
     """
 
-    def __init__(self, patterns, insigns, outsigns):
+    def __init__(self, patterns, insigns, outsigns, debug=False):
+
         def compute_number_of_states(xs):
             k = np.prod([len(x[0]) + 1 for x in xs])
-            m = np.max([len(x[1]) for x in xs])
-            return k * (len(xs) + 1) * m + 1
+            m = max(1, np.max([len(x[1]) for x in xs]) - 1)
+            return k * (len(xs) + 1) * m
 
         super().__init__(
             Q=compute_number_of_states(xs=patterns),
@@ -33,20 +34,28 @@ class PatternSwap(MealyMachine):
         self.n = len(self.patterns)
         self.NO_PRINTNIG = 0
         self.state_mapping = dict()
-        self.garbage = -1
+        self.debug = debug
         self._compute_state_transitions()
         self._compte_output_function()
 
+    """
+    zwraca długość:
+        i == 1 k-tego zamiennika 
+        i == 0 k-tego wzorca 
+    """
+
+    def sz(self, k, i=1):
+        return len(self.patterns[k][i])
+
     def _compute_state_transitions(self):
         def find_new_state(x, a):
-            print(f"stan: {x}, litera: {a}")
             printing, letter = x[self.n], x[self.n + 1]
-            if (
-                printing != self.NO_PRINTNIG
-                and letter == len(self.patterns[printing - 1][1]) - 1
-            ):
-                # print(f"ostani aliterka do wypisania !")
-                printing = self.NO_PRINTNIG
+            if printing != self.NO_PRINTNIG:
+                # jeśli to ostatnia literka - zmień status priniting
+                if letter >= self.sz(printing - 1) - 1:
+                    printing, letter = self.NO_PRINTNIG, 0
+                else:
+                    letter += 1
 
             words = [self.patterns[i][0][: x[i]] + a for i in range(self.n)]
             state, full_patterns = [], []
@@ -69,90 +78,44 @@ class PatternSwap(MealyMachine):
                 len(full_patterns) <= 1
             ), "wykryto kilka wzorców naraz w jednym miejscu - niewiadomo co wypisać!"
 
-            print(f"full_patterns = {full_patterns}")
-            if not (printing == self.NO_PRINTNIG or len(full_patterns) == 0):
-                print(f"zwracam stan: ŚMIETNIK (konflikt przy wypisywaniu)")
-                return self.state_mapping[self.garbage]
-
             if printing == self.NO_PRINTNIG:
-                if full_patterns:
-                    printing = full_patterns[0]
-                state.extend([printing, 0])
-            else:
-                state.extend([printing, letter + 1])
-
-            print(f"zwracam stan: {state}")
+                if len(full_patterns) == 1:
+                    printing, letter = full_patterns[0], 0
+            state.extend([printing, letter])
             return self.state_mapping[tuple(state)]
 
-        xs = [range(len(x[0]) + 1) for x in self.patterns]
+        xs = [range(self.sz(k, i=0) + 1) for k in range(self.n)]
         xs.append(range(self.n + 1))
-        xs.append(range(max([len(x[0]) for x in self.patterns])))
+        xs.append(range(max([self.sz(k, i=1) for k in range(self.n)])))
 
         match self.n:
             case 1:
                 nested_loop1, nested_loop2 = product(xs[0], xs[1], xs[2]), product(
                     xs[0], xs[1], xs[2]
                 )
+            case 2:
+                nested_loop1, nested_loop2 = product(
+                    xs[0], xs[1], xs[2], xs[3]
+                ), product(xs[0], xs[1], xs[2], xs[3])
             case _:
-                assert False, "too much patterns, maximum possible is only 1!"
-
-        print(f"krotki reperzentujące stany:")
+                assert False, "too much patterns, maximum possible is only 2!"
         cnt = 0
         for x in nested_loop1:
             self.state_mapping[x] = cnt
-            print(x)
             cnt += 1
-        self.state_mapping[self.garbage] = cnt
-        cnt += 1
-        print(f"+ garbage , cnt = {cnt}")
-        for a in self.input_signs:
-            self.δ[(self.state_mapping[self.garbage], a)] = self.state_mapping[
-                self.garbage
-            ]
-            print(f"dodaje przejscie {self.state_mapping[self.garbage]}, {a}")
         for x in nested_loop2:
             for a in self.input_signs:
                 self.δ[(self.state_mapping[x], a)] = find_new_state(x, a)
 
     def _compte_output_function(self):
-
-        print(f"#stanów to {self.Q}")
         for i in range(self.Q):
             for a in self.input_signs:
-                # print(f"obliczam lambda({i}, {a}) ")
                 q = list(self.state_mapping.keys())[
                     list(self.state_mapping.values()).index(i)
                 ]
-                if q == self.garbage:
-                    self.λ[(i, a)] = "0"
-                    # print(f"dodaje funkcje wyjśćiową {i}, {a}")
+                printing, letter = q[self.n], q[self.n + 1]
+                if printing != self.NO_PRINTNIG and letter < self.sz(printing - 1):
+                    letter = self.patterns[printing - 1][1][letter]
                 else:
-                    printing, letter = q[self.n], q[self.n + 1]
-                    if printing == self.NO_PRINTNIG:
-                        letter = "0"
-                    else:
-                        letter = self.patterns[printing - 1][1][letter]
-                    self.λ[(i, a)] = letter
-
-    def route(self, w=10):
-        w = (
-            "".join(np.random.choice(self.input_signs, w, replace=True))
-            if isinstance(w, int)
-            else w
-        )
-        v, q = "", 0
-
-        # print(f"v = {v}, w = {w}")
-        for a in w:
-            # print(f"czytam a = {a}")
-            assert (q, a) in self.λ and (
-                q,
-                a,
-            ) in self.δ, "nie ma taiego przejścia w maszynie, potencjalnie zły alfabet!"
-            v += self.λ[(q, a)]
-            q = self.δ[(q, a)]
-            # print(
-            #     f"q = {list(self.state_mapping.keys())[list(self.state_mapping.values()).index(q)]}, v = {v}"
-            # )
-            assert q != self.state_mapping[self.garbage], "konflikt przy wypisywaniu !"
-        return (w, v)
+                    letter = a
+                self.λ[(i, a)] = letter
