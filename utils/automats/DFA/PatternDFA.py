7d7117a9400bf47fd747167bc0e2b3b5fc6d1308
michalfica <michalfica125@gmail.com>
2024-12-12 09:10:16 +0100

    Pattern Detection OR type

--- utils/automats/DFA/PatternDFA.py
+++ utils/automats/DFA/PatternDFA.py
@@ -7,7 +7,11 @@ from itertools import product
 
 
 class PatternDFA(DFA):
-    def __init__(self, input_signs, patterns):
+    AND_TYPE_PATTERN_DFA = "AND"
+    OR_TYPE_PATTERN_DFA = "OR"
+    EMPTY_STRING = ""
+
+    def __init__(self, input_signs, patterns, _type=AND_TYPE_PATTERN_DFA):
         def compute_number_of_states():
             k = 1
             for x in patterns:
@@ -17,11 +21,20 @@ class PatternDFA(DFA):
         super().__init__(
             Q=compute_number_of_states(), input_signs=input_signs, δ=dict(), F=set()
         )
+
+        patterns.sort(key=lambda s: -len(s))
+        if patterns[-1] == PatternDFA.EMPTY_STRING:
+            patterns.pop()
+            self.mark_start_state_as_accepting = True
+        else:
+            self.mark_start_state_as_accepting = False
+
         self.patterns = patterns
         self.n = len(patterns)
         self.state_mapping = dict()
+        self.type = _type
         self._compute_state_transitions()
-        self._mark_accepting_states()  # and correct transtion function
+        self._mark_accepting_states()
 
     def _compute_state_transitions(self):
         def find_new_state(x, a):
@@ -37,7 +50,7 @@ class PatternDFA(DFA):
                     if s in prefixes:
                         state.append(len(s) % (len(self.patterns[i]) + 1))
                         break
-            # korekta
+            # korekta, zapamiętuję, że patterns[i] juz wystąpił
             for i in range(self.n):
                 if x[i] == len(self.patterns[i]):
                     state[i] = x[i]
@@ -56,6 +69,20 @@ class PatternDFA(DFA):
                 self.δ[(self.state_mapping[x], a)] = find_new_state(x, a)
 
     def _mark_accepting_states(self):
-        state = [len(x) for x in self.patterns]
-        accept_state = self.state_mapping[tuple(state)]
-        self.F.add(accept_state)
+        if self.mark_start_state_as_accepting:
+            start_state = self.state_mapping[tuple([0 for _ in range(self.n)])]
+            self.F.add(start_state)
+
+        if self.type == PatternDFA.AND_TYPE_PATTERN_DFA:
+            state = [len(x) for x in self.patterns]
+            accept_state = self.state_mapping[tuple(state)]
+            self.F.add(accept_state)
+        elif self.type == PatternDFA.OR_TYPE_PATTERN_DFA:
+            xs = [range(len(x) + 1) for x in self.patterns]
+            nested_loop = product(*xs)
+            for state in nested_loop:
+                for i in range(self.n):
+                    if state[i] == len(self.patterns[i]):
+                        self.F.add(self.state_mapping[tuple(state)])
+        else:
+            assert False, "Wrong type of PatternDFA, 'AND' or 'OR' type allowed only!"
