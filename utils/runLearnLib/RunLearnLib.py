80365c7a89dad0ec05010849aa87e47e3760221b
michalfica <michalfica125@gmail.com>
2025-07-15 15:13:54 +0200

    RunearnLib development

--- utils/runLearnLib/RunLearnLib.py
+++ utils/runLearnLib/RunLearnLib.py
@@ -6,7 +6,27 @@ Allows to:
     * returns the result of execution of the above algorithms and handles possible errors.
 """
 
+import subprocess
+
 
 class RunLearnLib:
+
     def __init__(self):
         pass
+
+    """
+    TO DO: 
+        * przymyśleć, CO mozę sie wydarzyć jakie błędy mgą wytąpić podczas kompilacji?
+        * jakie błędy obsłużyć?
+        * Czy to dobra ścieżka do uruchomienia?
+    """
+
+    def compileLearnLib(self):
+        _ = subprocess.run(
+            [
+                "cd ../../../learnlib/examples ; mvn clean install; cd ../../magisterka/test_algorithm/Lstar"
+            ],
+            shell=True,
+            capture_output=True,
+            text=True,
+        )
