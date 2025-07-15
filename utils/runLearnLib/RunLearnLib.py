e459bcf9c5fad01a6b9affe29db738567c2df4be
michalfica <michalfica125@gmail.com>
2025-07-15 15:05:21 +0200

    new calss to communicate with LearnLib

---
+++ utils/runLearnLib/RunLearnLib.py
@@ -0,0 +1,13 @@
+"""
+Class that is used to communicate with LearnLib repository.
+Allows to:
+    * compile LearLib repository,
+    * run LearnLib implementation of L* or TTT algorithm,
+    * returns the result of execution of the above algorithms and handles possible errors.
+"""
+
+
+class RunLearnLib:
+
+    def __init__():
+        pass
