bb4c09b8c2b06495aaab4bdfdab282584edd020e
michalfica <michalfica125@gmail.com>
2025-07-17 20:40:03 +0200

    usage of subprocess hidden from user

--- utils/runLearnLib/RunLearnLib.py
+++ utils/runLearnLib/RunLearnLib.py
@@ -10,42 +10,56 @@ Allows to:
 
 
 class RunLearnLib:
+    """
+    TO DO:
+        * czy dawać obsługiwać bardziej szczegółowo błędy?
+        * Czy to odpowiednia ścieżka do uruchomienia?
+    """
 
     COMPILATION_ERR = "[ERROR] COMPILATION ERROR :"
 
     def __init__(self, debug=False):
         self.debug = debug
 
-    """
-    TO DO: 
-        * przymyśleć, CO mozę sie wydarzyć jakie błędy mgą wytąpić podczas kompilacji?
-        * jakie błędy obsłużyć?
-        * Czy to dobra ścieżka do uruchomienia?
-    """
-
-    # WĄTPLIWOŚĆI:
-    #  * czy komenda jest poprawna, czy powinienem zmieniać ścieżkę?
-
-    # TO DO:
-    # * uruchomienie liku z Learnliba i zwrócenie wyniku
-    def compileLearnLib(self):
-        proc = subprocess.run(
-            ["cd ../../../learnlib/examples ; mvn clean install"],
-            shell=True,
-            capture_output=True,
-            text=True,
+    def __runCommand(self, command):
+        proc = subprocess.run([command], shell=True, capture_output=True, text=True)
+        assert proc.returncode == 0, (
+            "Error during execution of subprocess.run "
+            + command
+            + "\nstout: "
+            + proc.stdout
+            + "\nstderr: "
+            + proc.stderr
         )
+        return proc
 
-        proc_output = proc.stdout
-        if proc.returncode != 0:
-            compErr = proc_output.find(self.COMPILATION_ERR)
-            if compErr != -1:
-                assert False, "Compilation error during 'mvn clean install'"
-            else:
-                assert False, (
-                    "Error during execution of subprocess.run"
-                    + "\nstout: "
-                    + proc.stdout
-                    + "\nstderr: "
-                    + proc.stderr
-                )
+    def compileLearnLib(self):
+        c = "cd ../../../learnlib/examples ; mvn clean install"
+        _ = self.__runCommand(c)
+
+        # output = proc.stdout
+        # if proc.returncode != 0:
+        #     compErr = output.find(self.COMPILATION_ERR)
+        #     if compErr != -1:
+        #         assert False, "Compilation error during 'mvn clean install'"
+
+    def __getNumberofEQ(self, s):
+        for l in s.splitlines():
+            if l.startswith("Learning rounds"):
+                return int(l.split()[-1])
+
+    def runLstar(self):
+        c = 'cd ../../../learnlib/examples ; mvn exec:java -q -Dexec.mainClass="de.learnlib.example.LstarExample1"'
+        proc = self.__runCommand(c)  # ; cd ../../magisterka/test_algorithm/Lstar
+
+        mq = "?"
+        eq = self.__getNumberofEQ(proc.stdout)
+        return [mq, eq]
+
+    def runLstarWithAdvice(self):
+        c = 'cd ../../../learnlib/examples ; mvn exec:java -q -Dexec.mainClass="de.learnlib.example.LstarExample_conv_cl_withAS"'
+        proc = self.__runCommand(c)  # ; cd ../../magisterka/test_algorithm/Lstar
+
+        mq = "?"
+        eq = self.__getNumberofEQ(proc.stdout)
+        return [mq, eq]
